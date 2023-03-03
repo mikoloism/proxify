506b05ffbcdda7bbf10653edf3e712b8475961ed
mikoloism <mikoloism@gmail.com>
2023-03-03 21:56:21 +0330

    fix(renderer-process): update components;l
    rename `primary` to `blue`;

--- src/renderer/components/atoms/TopBar.vue
+++ src/renderer/components/atoms/TopBar.vue
@@ -1,5 +1,5 @@
 <template>
 	<nav
-		class="w-screen h-12 border-primary-800 border-b items-center flex shadow-xl z-0 fixed top-0"
+		class="w-screen h-12 border-blue-800 border-b items-center flex shadow-xl z-0 fixed top-0"
 		style="-webkit-app-region: drag" />
 </template>
